8cb75eb398596198b25e24f062dfa946202ec61d
Dib Nasreddine <nasreddine.dib@alumni.univ-avignon.fr>
2017-04-19 14:38:09 +0200

    IpokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -2,13 +2,30 @@ package fr.univavignon.pokedex.api;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.*;
+import org.mockito.Mockito;
+
 
 public class IPokedexFactoryTest {
 
-	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	@Mock private IPokedexFactory pokedexFactory;
+	private IPokemonMetadataProvider pokemonMetadataProvider;
+	private IPokemonFactory pokemonFactory;
+	private IPokedex pokedex;
+
+	@Before
+	public void setUp() throws PokedexException {
+		MockitoAnnotations.initMocks(this);
+		Mockito.when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory)).thenReturn(pokedex);
 	}
+	
+	@Test
+	public void createPokedexTest() {
+		assertEquals(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory), pokedex);
+	}
+
+	
 
 }
