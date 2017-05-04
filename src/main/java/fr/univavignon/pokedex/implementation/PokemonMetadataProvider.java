d976ed5825059e53197fabb395c7a4cb796120f1
admin <nasreddine.dib@alumni.univ-avignon.fr>
2017-05-04 17:42:00 +0200

    Ajout de la classe PokemonMetadataProvider

---
+++ src/main/java/fr/univavignon/pokedex/implementation/PokemonMetadataProvider.java
@@ -0,0 +1,93 @@
+package fr.univavignon.pokedex.implementation;
+
+import java.io.IOException;
+/**
+ * @author Dib Nasreddine
+ *
+ */
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import fr.univavignon.pokedex.api.IPokemonMetadataProvider;
+import fr.univavignon.pokedex.api.PokedexException;
+import fr.univavignon.pokedex.api.PokemonMetadata;
+
+public class PokemonMetadataProvider implements IPokemonMetadataProvider, Serializable {
+
+	private static final long serialVersionUID = -1331137594371941987L;
+	private static PokemonMetadataProvider INSTANCE = null;
+	private List<PokemonMetadata> PokemonMetadataList;
+	private static String URL_STRING = "https://raw.githubusercontent.com/PokemonGo-Enhanced/node-pokemongo-data/master/data.json";
+
+	/**
+	 * Retourne le singleton
+	 * @return PokemonMetadataProvider
+	 */
+	public static synchronized PokemonMetadataProvider getInstance() throws JSONException, IOException{
+		return INSTANCE == null?new PokemonMetadataProvider():INSTANCE;
+	}
+
+	/**
+	 * Contructeur privee qui initialise la liste et la rempli a partir du fichier json
+	 * @throws JSONException
+	 * @throws IOException
+	 */
+	private PokemonMetadataProvider() throws JSONException, IOException{
+		this.PokemonMetadataList = new ArrayList<PokemonMetadata>();
+		this.fillPokemonMetadataList();
+	}
+
+	/**
+	 * Rempli la liste PokemonMetadataList avec les PokemonMetadata recupere et generes a partir du fichier json
+	 * @throws JSONException
+	 * @throws IOException
+	 */
+	private void fillPokemonMetadataList() throws JSONException, IOException {
+		URL url = new URL(URL_STRING);
+		JSONArray jsonArray = new JSONArray(IOUtils.toString(url));
+		for(int i = 0; i < jsonArray.length(); i++){
+			JSONObject obj = jsonArray.optJSONObject(i);
+			PokemonMetadata pmdt = jsonToPMDT(obj);
+			PokemonMetadataList.add(pmdt);
+		}
+	}
+
+	/**
+	 * Retourne un objet PokemonMetadata correspondant a l''index passe en parametre
+	 * @param data
+	 * @return PokemonMetadata
+	 * @throws JSONException
+	 */
+	@Override
+	public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
+		PokemonMetadata result = null;
+		if(index < 0 || index > 150)
+			throw new PokedexException("L'index doit etre compris entre 0 et 150");
+
+		return result;
+	}
+
+	/**
+	 * Cree un PokemonMetadata a partir d'un objet json passe en parametre
+	 * @param data
+	 * @return PokemonMetadata
+	 * @throws JSONException
+	 */
+	public PokemonMetadata jsonToPMDT(JSONObject data) throws JSONException{
+		if(data == null) throw new JSONException("data can't be null");
+		PokemonMetadata pokemonMetadata;
+		pokemonMetadata = new PokemonMetadata(data.getInt("PkMn")-1,
+				data.getString("Identifier"), 
+				data.getInt("BaseAttack"), 
+				data.getInt("BaseDefense"), 
+				data.getInt("BaseStamina"));
+		return pokemonMetadata;
+	}
+
+}
