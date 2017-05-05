eecc44812613f49bdb4ca591b82dee386da01dd6
admin <nasreddine.dib@alumni.univ-avignon.fr>
2017-05-05 07:26:21 +0200

    PokemonFactory modif minimes

--- src/main/java/fr/univavignon/pokedex/implementation/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/implementation/PokemonFactory.java
@@ -23,7 +23,7 @@ public class PokemonFactory implements IPokemonFactory, Serializable {
 	private static PokemonFactory INSTANCE;
 
 	private PokemonFactory() {}
-	
+
 	public static synchronized PokemonFactory getInstance() {
 		return INSTANCE == null?new PokemonFactory():INSTANCE;
 	}
@@ -32,26 +32,14 @@ public class PokemonFactory implements IPokemonFactory, Serializable {
 	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
 
 		PokemonMetadataProvider pmdtp;
-
-		try {
-			pmdtp = PokemonMetadataProvider.getInstance();
-		} catch (PokedexException e) {
-			e.printStackTrace();
-			return null;
-		}
-
 		PokemonMetadata pmdt;
-
 		try {
+			pmdtp = PokemonMetadataProvider.getInstance();		
 			pmdt = pmdtp.getPokemonMetadata(index);
 		} catch (PokedexException e) {
-			e.printStackTrace();
 			return null;
 		}
-
-		String name = pmdt.getName();
-		Pokemon pokemon = new Pokemon(index, pmdt.getName(), pmdt.getAttack(), pmdt.getDefense(), pmdt.getStamina(), cp, hp, dust, candy, calculIV(name, cp, dust));
-		return pokemon;
+		return new Pokemon(index, pmdt.getName(), pmdt.getAttack(), pmdt.getDefense(), pmdt.getStamina(), cp, hp, dust, candy, calculIV(pmdt.getName(), cp, dust));
 	}
 
 	/**
